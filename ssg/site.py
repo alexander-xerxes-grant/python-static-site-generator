2630704f961ed035d843544b484a89cc7c8a710b
Alexander Grant <alexander.grant@dunelm.com>
2022-11-01 07:41:24 +0000

    create for loop in build method

--- ssg/site.py
+++ ssg/site.py
@@ -12,4 +12,6 @@ class Site:
 
     def build(self):
         self.dest.mkdir(parents=True, exist_ok=True)
-        
+        for path in self.source.rglob("*"):
+            if path.isdir():
+                self.create_dir(path)
