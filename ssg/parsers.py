45b783f7e797aac46eb3b0d5548174dc5c52176a
Alexander Grant <alexander.grant@dunelm.com>
2022-11-02 07:32:47 +0000

    test

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -3,4 +3,16 @@ from pathlib import Path
 
 class Parser:
     
-       extensions: List[str] = []
+    extensions: List[str] = []
+
+    def valid_extension(self, extension):
+        if extension in self.extensions():
+            return extension
+    
+    def parse(self, path: Path, source: Path, dest: Path):
+        raise NotImplementedError
+
+    def read(self, path: Path):
+        with open(path) as file:
+            return file.read()
+    
