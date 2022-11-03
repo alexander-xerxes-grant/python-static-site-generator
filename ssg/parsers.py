22c793b811b0c60b470f6f76f4965ab4c899b09d
Alexander Grant <alexander.grant@dunelm.com>
2022-11-03 16:59:26 +0000

    Remove if block

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -6,8 +6,7 @@ class Parser:
     extensions: List[str] = []
 
     def valid_extension(self, extension):
-        if extension in self.extensions():
-            return extension
+        return extension in self.extensions
     
     def parse(self, path: Path, source: Path, dest: Path):
         raise NotImplementedError
@@ -18,7 +17,3 @@ class Parser:
     
     def write(self, path: Path, dest: Path, content, ext=".html"):
         full_path = dest / path.with_suffix(ext).name
-    
-    def valid_extension(self,extension):
-        if extension in self.extensions:
-            return True
