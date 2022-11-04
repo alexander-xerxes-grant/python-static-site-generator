e9fd6ba39620b16e861057ac974ca69e7642549c
Alexander Grant <alexander.grant@dunelm.com>
2022-11-04 07:34:35 +0000

    Create ResourceParser subclass

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -21,7 +21,12 @@ class Parser:
         with open(full_path, 'w') as file:
             file.write(content)
     
-    def copy(self, path, source, dest):
+    def copy(self, path: Path, source: Path, dest: Path):
         shutil.copy2(path, dest / path.relative_to(source))
 
-    
+class ResourceParser(Parser):
+
+    extensions = [".jpg", ".png", ".gif", ".css", ".html"]
+
+    def parse(self, path: Path, source: Path, dest: Path):
+        self.copy(path, source, dest)
