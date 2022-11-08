74ec56a0ceaf144afa937852ed55f45a743bfd8b
Alexander Grant <alexander.grant@dunelm.com>
2022-11-08 06:40:15 +0000

    Create Content class

---
+++ ssg/content.py
@@ -0,0 +1,20 @@
+import re
+
+from yaml import load, FullLoader
+
+from collections.abc import Mapping
+
+
+class Content(Mapping):
+
+    __delimeter = "^(?:-|\+){3}\s*$"
+
+    __regex = re.compile(__delimeter, re.MULTILINE)
+
+    @classmethod
+    def load(cls, string):
+        _, fm, content =  cls.__regex.split(string, 2)
+        metadata = load(fm, Loader=FullLoader)
+        return cls(metadata, content)
+
+    
